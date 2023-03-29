7ef04f57553d0c2a5a16d1e92519747db911a0c0
1861784 <1861784@01-19851.pusd.dom>
2023-03-29 10:48:13 -0700

    Done with SinglyLinked Lists

--- LinkedList/src/LinkedListTester.java
+++ LinkedList/src/LinkedListTester.java
@@ -13,10 +13,27 @@ class LinkedListTester {
 		temp.add(new Node(2));
 		temp.add(new Node(3));
 		temp.add(new Node(4));
+		temp.add(new Node(5));
+		temp.add(new Node(6));
 		
-	
 		
-		System.out.println(temp.toString());
+		
+		System.out.println("Original: " + temp.toString());
+		
+		System.out.println("Removed: " + temp.remove(3).getData());
+		System.out.println("Size: " + temp.size());
+		
+		System.out.println("New: " + temp.toString());	
+		
+		System.out.println("Check index 1: " + temp.get(1).getData());
+		System.out.println("Check index 3: " + temp.get(3).getData());
+		System.out.println("Check index 5: " + temp.get(5).getData());
+		
+		System.out.println("Removed: " + temp.remove(5).getData());
+		
+		System.out.println("New: " + temp.toString());	
+		
+		System.out.println(temp.size());
 		
 	
 		
--- LinkedList/src/SinglyLinkedList.java
+++ LinkedList/src/SinglyLinkedList.java
@@ -5,58 +5,79 @@ public class SinglyLinkedList {
 	Node head;
 	Node tail;
 	private int size;
-	
-	
+
 	public SinglyLinkedList() {
-		size = 0; 
+		size = 0;
 		head = null;
 		tail = null;
 	}
-	
+
 	public int size() {
 		return size;
 	}
-	
+
+	Node get(int index) { // currently returning node address instead of value
+
+		Node n = head;
+
+		for (int i = 1; i < index; i++) {
+			n = n.next;
+		}
+
+		return n;
+
+	}
+
 	public String toString() {
 		/* to be completed by student */
 		String result = "";
 		Node n = head;
-		
-		while(n != null) {
+
+		while (n != null) {
 			result += n.getData() + " ";
 			n = n.next;
 		}
-		
+
 		return result;
-		
 	}
-	
-	
-//	public Node remove(int n) {
-//		/*to be completed by student */
-//		
-//		
-//		
-//		
-//		
-//		size -= 1;
-//	
-//	}
-	
+
+	public Node remove(int target) {
+		/* to be completed by student */
+
+		Node n = head;
+		Node temp;
+		Node remove;
+
+		for (int i = 1; i < target-1; i++) {
+			n = n.next;
+		}
+		
+		//set temp = to 2 nodes later 
+		temp = n.next;  
+		remove = n.next;  //this n.next is the node you remove
+		temp = temp.next;
+		
+		//set n.next = to the node after skip 
+		n.next = temp;
+		size -= 1;
+		
+		//return the value you removed
+		return remove;
+
+	}
+
 	public void add(Node n) {
 		/* to be completed by student */
-		if(tail == null) {
+		if (tail == null) {
 			head = tail = n;
-		}else {
+		} else {
 			tail.next = n;
 			tail = n;
 		}
-		
+
 		tail.next = null;
-		
+
 		size += 1;
 	}
-	
- 
-	
+
 }
